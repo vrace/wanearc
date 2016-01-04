27a57816a8ac7aad74f187ab267b5f03f2afb50f
vrace <vrace_studios@hotmail.com>
2016-01-04 06:33:53 -0800

    add testing project for wafext

---
+++ wafext_test/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../wafext/wafext.h"
+
+static int unit_restore(struct waf_archive_setup *setup, unsigned char *restored, const unsigned char *transformed, int size)
+{
+	memcpy(restored, transformed, size);
+	return size;
+}
+
+static struct waf_archive_setup unit_setup =
+{
+	32 * 1024,
+	32 * 1024,
+	unit_restore,
+	{ 'R', 'A', 'W', 0 },
+};
+
+int main(void)
+{
+	struct waf_archive *arc;
+
+	arc = waf_open_archive("../wanearc/data.waf", &unit_setup);
+	if (arc)
+	{
+		printf("Archive opened!\n");
+
+		waf_close_archive(arc);
+	}
+	else
+	{
+		printf("Unable to open archive.\n");
+	}
+
+	return 0;
+}
